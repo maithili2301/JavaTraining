9ec09714b088aa7345e611fac39dcb108b04fe72
Maithili <82694210+maithili2301@users.noreply.github.com>
2023-07-21 17:36:12 +0530

    Add files via upload

---
+++ JungleProject/src/jungle/cave/Tiger.java
@@ -0,0 +1,14 @@
+package jungle.cave;
+
+public class Tiger {
+	public Tiger() {
+		
+	}
+   void roar() {
+	   System.out.println("Hey tiger is roaring");
+   }
+   public void walk() {
+	   System.out.println("Tiger is walking");
+   }
+   
+}
