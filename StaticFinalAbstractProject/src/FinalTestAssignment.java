19cedb9e7122d09bb2548cbd6eb137c42afdd192
Maithili <82694210+maithili2301@users.noreply.github.com>
2023-07-26 09:53:00 +0530

    Add files via upload

--- StaticFinalAbstractProject/src/FinalTestAssignment.java
+++ StaticFinalAbstractProject/src/FinalTestAssignment.java
@@ -2,76 +2,123 @@
 public class FinalTestAssignment {
 
 	public static void main(String[] args) {
-		
-		Area areaObj=new Area();
-		areaObj.houseNo="106F";
-		areaObj.SocietyName="Vignesh vertex";
-		areaObj.setAreaName("wakad");
-		areaObj.setPinCode("412111");
-		areaObj.setCity("Pune");
-		
+
+		Area areaObj = new Area();
+		areaObj.houseNo = "106F";
+		areaObj.SocietyName = "Vignesh vertex";
+
 		areaObj.showAddress();
 		System.out.println("----------------------");
-		areaObj.showArea();
-		
-		
+		Area.showArea();
+		System.out.println("-----------------------");
+
+		StateCountry stateCountryObj = new StateCountry();
+
+		stateCountryObj.showStateCountry();
+		System.out.println("==================================================");
+
+		Area areaObj1 = new Area();
+		areaObj1.houseNo = "107F";
+		areaObj1.SocietyName = "Vignesh Vertex 2";
+		areaObj1.showAddress();
+		System.out.println("------------------------------------");
+		Area.showArea();
+		System.out.println("---------------------------------------");
+		StateCountry stateCountryObj1 = new StateCountry();
+		stateCountryObj1.showStateCountry();
+		System.out.println("==================================================");
 
 	}
 
 }
 
-class Address{
+class Address {
 	String houseNo;
 	String SocietyName;
+
 	public String getHouseNo() {
 		return houseNo;
 	}
+
 	public void setHouseNo(String houseNo) {
 		this.houseNo = houseNo;
 	}
+
 	public String getSocietyName() {
 		return SocietyName;
 	}
+
 	public void setSocietyName(String societyName) {
 		SocietyName = societyName;
 	}
+
 	void showAddress() {
-		System.out.println("House No : "+getHouseNo() );
-		System.out.println("Society Name : "+getSocietyName());
+		System.out.println("House No : " + getHouseNo());
+		System.out.println("Society Name : " + getSocietyName());
 	}
-	
+
 }
-final class Area extends Address{
-	private String areaName;
-	private String pinCode;
-	private String City;
-	public String getAreaName() {
+
+class Area extends Address {
+	private static String areaName;
+	private static String pinCode;
+	private static String City;
+
+	public static String getAreaName() {
 		return areaName;
 	}
-	public void setAreaName(String areaName) {
-		this.areaName = areaName;
+
+	public static void setAreaName(String areaName) {
+		Area.areaName = "Wakad";
 	}
-	public String getPinCode() {
+
+	public static String getPinCode() {
 		return pinCode;
 	}
-	public void setPinCode(String pinCode) {
-		this.pinCode = pinCode;
+
+	public static void setPinCode(String pinCode) {
+		Area.pinCode = "412311";
 	}
-	public String getCity() {
+
+	public static String getCity() {
 		return City;
 	}
-	public void setCity(String city) {
-		City = city;
+
+	public static void setCity(String city) {
+		Area.City = "Pune";
 	}
-	
-	void showArea() {
-		System.out.println("Arrea : "+getAreaName());
-		System.out.println("Pincode : "+getPinCode());
-		System.out.println("City : "+getCity());
+
+	static void showArea() {
+		System.out.println("Arrea : " + Area.getAreaName());
+		System.out.println("Pincode : " + Area.getPinCode());
+		System.out.println("City : " + Area.getCity());
 	}
-	
-	
+
 }
 
+final class StateCountry {
+	private String state="Maharashtra";
+	private String country="India";
 
+//	public String getState() {
+//		return state;
+//	}
+//
+//	public void setState(String state) {
+//		this.state = "Maharashtra";
+//	}
+//
+//	public String getCountry() {
+//		return country;
+//	}
+//
+//	public void setCountry(String country) {
+//		this.country = "India";
+//	}
 
+	void showStateCountry() {
+		System.out.println("State  : " +state );
+		System.out.println("Country : " + country);
+	}
+
+}
